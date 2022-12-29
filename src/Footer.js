608e9a50ee74fbd838652b1c063f11ebac2a326e
jainam03 <jbchheda03@gmail.com>
2022-12-30 00:03:21 +0530

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h3>Made with 💙 using React JS</h3>
+            <br />
+            <a href="https://www.youtube.com" target="blank" >
+                <i class="fa fa-github fa-2x " id="icon" width="40px" href="https://github.com/jainam03/weather-react-app" ></i>
+            </a>
+        </div>
+    )
+}
