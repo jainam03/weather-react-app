1f4c9a84fcc993782f07ff9246836826dd847798
jainam03 <jbchheda03@gmail.com>
2023-01-01 23:00:24 +0530

    main functionality is up and running now

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ function App() {
   const [weather, setWeather] = useState({});
   const [notFoundError, setNotFoundError] = useState(false)
 
-  // const search = evt => {
-  //   if (evt.key === "Enter") {
-  //     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-  //       .then(res => res.json())
-  //       .then(result => {
-  //         if (result.code === 404) {
-  //           setNotFoundError(true)
-  //         } else {
-  //           setWeather(result)
-  //           setQuery('')
-  //         }
-  //       })
-  //   }
-  // }
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          if (result.code === 404) {
+            setNotFoundError(true)
+          } else {
+            setWeather(result)
+            setQuery('')
+          }
+        })
+    }
+  }
 
   // const search = evt => {
   //   if (evt.key === "Enter") {
@@ -45,17 +45,17 @@ function App() {
   //   }
   // }
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
-  }
+  // const search = evt => {
+  //   if (evt.key === "Enter") {
+  //     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         setWeather(result);
+  //         setQuery('');
+  //         console.log(result);
+  //       });
+  //   }
+  // }
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
